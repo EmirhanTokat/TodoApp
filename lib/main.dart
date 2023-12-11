99d44b37c84529abd958d2b43abf430ac7a2d7ef
Emirhan <emirhantokatt@gmail.com>
2023-12-11 10:42:19 +0300

    Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:todo_app/screens/home_screen.dart';
+import 'package:todo_app/todo_bloc/todo_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +18,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Todo App",
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          background: Colors.white,
+          onBackground: Colors.black,
+          primary: Colors.yellowAccent,
+          onPrimary: Colors.black,
+          secondary: Colors.lightGreen,
+          onSecondary: Colors.white,
+        ),
+      ),
+      home: BlocProvider<TodoBloc>(
+        create: (context) => TodoBloc()..add(TodoStarted()),
+        child: const HomeScreen(),
+      ),
+    );
   }
 }
